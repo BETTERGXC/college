9d02fe9b2100226a78d408d442a88a31738216be
BetterGXC <2542602168@qq.com>
2020-02-07 21:03:30 +0800

    计算1/1-1/2+1/3-1/4+1/5 …… + 1/99 - 1/100 的值

---
+++ 计算11-12+13-14+15 …… + 199 - 1100 的值.c
@@ -0,0 +1,16 @@
+
+//¼ÆËã1/1-1/2+1/3-1/4+1/5 ¡­¡­ + 1/99 - 1/100 µÄÖµ 
+
+
+
+int main() {
+	float sum = 0.0;
+	int i;
+	for (i = 1; i <= 100; i++) {
+		sum = sum + (pow(-1, i+1) * 1 / i);
+	}
+	printf("%f\n", sum);
+	system("pause");
+	return 0;
+
+
