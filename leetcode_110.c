9e5cc2dd040cd2df7e74f6fb608ffd3b9336bcfe
BETTERGXC <2542602168@qq.com>
2020-03-11 11:38:30 +0800

    平衡二叉树

--- leetcode_110.c
+++ leetcode_110.c
@@ -1,3 +1,7 @@
+//  平衡二叉树 
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -19,4 +23,26 @@ int TreeHigh(struct TreeNode* root) {
 }
 bool isBalanced(struct TreeNode* root){
     return TreeHigh(root) != -1;
+}
+
+
+// 第二种 
+int Depth(struct TreeNode* root){                        //   算深度 
+    if(root == NULL)
+        return 0;
+    int leftDepth = Depth(root->left);
+    int rightDepth = Depth(root->right);
+
+    return leftDepth>rightDepth?leftDepth+1:rightDepth+1;
+}
+
+bool isBalanced(struct TreeNode* root) {
+    if(root == NULL)
+        return true;
+    int leftDepth = Depth(root->left);
+    int rightDepth = Depth(root->right);
+
+    return abs(leftDepth-rightDepth)<2                 //  如果两树的深度差小于二且左数平衡右数也平衡则为平衡二叉树 
+            && isBalanced(root->left)
+            && isBalanced(root->right);
 }
