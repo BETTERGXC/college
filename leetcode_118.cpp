c6cf7012d21aa07d78949d273c9ae98741ac2e4e
BETTERGXC <2542602168@qq.com>
2020-05-28 10:16:01 +0800

    杨辉三角

---
+++ leetcode_118.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> vv;
+        vv.resize(numRows);
+        for(int i = 1;i <= numRows;++i) {
+            vv[i-1].resize(i,0);
+            vv[i-1][0] = 1;
+            vv[i-1][i-1] = 1;
+        }
+        for(int i = 0;i < vv.size();++i) {
+            for(int j = 0;j < vv[i].size();++j) {
+                if(vv[i][j] == 0) {
+                    vv[i][j] = vv[i-1][j-1] + vv[i-1][j];
+                }
+            }
+        }
+        return vv;
+    }
+};
