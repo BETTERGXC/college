fde93f2067575f57cec74f4618fa895184e27bf5
BETTERGXC <2542602168@qq.com>
2020-08-12 21:12:59 +0800

    变态青蛙跳台阶

---
+++ nowcoder_day26_1.cpp
@@ -0,0 +1,13 @@
+// 变态青蛙跳台阶
+
+class Solution {
+public:
+    int jumpFloorII(int number) {
+        if(number <= 0)
+            return -1;
+        if(number == 1)
+            return 1;
+        if(number > 1)
+            return 2*jumpFloorII(number - 1);
+    }
+};
