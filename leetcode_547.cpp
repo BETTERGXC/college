ad295dff4d881862fa0548bd5b8560912a085d0a
BETTERGXC <2542602168@qq.com>
2020-09-05 20:59:48 +0800

    朋友圈

---
+++ leetcode_547.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    class UnionFindSet {
+public :
+	UnionFindSet(int n) {
+		_v.resize(n,-1);
+	}
+
+	int FindRoot(int x) {
+		while (_v[x] >= 0) {
+			x = _v[x];
+		}
+		return x;
+	}
+
+	bool Union(int x1,int x2) {
+		int root1 = FindRoot(x1);
+		int root2 = FindRoot(x2);
+
+		// x1 和 x2 的根相同,本就在同一个集合中
+		if (root1 == root2)
+			return false;
+
+		_v[root1] += _v[root2];
+		_v[root2] = root1;
+		return true;
+	}
+
+    int Size() {
+        int n = 0;
+        for(int e : _v) {
+            if(e < 0)
+                ++n;
+        }
+        return n;
+    }
+
+private:
+	vector<int> _v;
+};
+    int findCircleNum(vector<vector<int>>& M) {
+        UnionFindSet ufs(M.size());
+        
+        for(int i = 0; i < M.size(); ++i) {
+            for(int j = 0; j < M[i].size(); ++j) {
+                if(M[i][j] == 1)
+                    ufs.Union(i,j);
+            }
+        }
+
+        return ufs.Size();
+    }
+};
