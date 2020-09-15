8e94acb9d30e6b100e13ab98850ecaf35c78c7d8
BETTERGXC <2542602168@qq.com>
2020-09-15 22:51:49 +0800

    二叉树的最大深度

---
+++ nowcoder_二叉树的最大深度.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    /**
+     * 
+     * @param root TreeNode类 
+     * @return int整型
+     */
+    int maxDepth(TreeNode* root) {
+        // write code here
+        if(!root)
+            return 0;
+        return (max(maxDepth(root->left),maxDepth(root->right))) + 1;
+    }
+};
