8016a112d1d065a68b7111958a5092eab08aba8b
BETTERGXC <2542602168@qq.com>
2020-09-22 21:31:34 +0800

    替换空格

---
+++ 剑指offer_05.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string replaceSpace(string s) {
+        string ret;
+        for(int i = 0; i < s.size(); ++i) {
+            if(s[i] == ' ') {
+                ret += "%20";
+            }
+            else {
+                ret += s[i];
+            }
+        }
+        return ret;
+    }
+};
