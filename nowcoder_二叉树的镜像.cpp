78b70d59975bac09e4c395646841f726336082ad
BETTERGXC <2542602168@qq.com>
2020-11-14 13:11:56 +0800

    二叉树的镜像

---
+++ nowcoder_二叉树的镜像.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void Mirror(TreeNode *pRoot) {
+        if(pRoot == nullptr)
+            return;
+        Mirror(pRoot->left);
+        Mirror(pRoot->right);
+        TreeNode* tmp = pRoot->left;
+        pRoot->left = pRoot->right;
+        pRoot->right = tmp;
+        
+        
+    }
+};
