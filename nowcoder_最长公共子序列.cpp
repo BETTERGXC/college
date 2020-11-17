2e4f6d5dd819c33cd75911da578316f3effbea0e
BETTERGXC <2542602168@qq.com>
2020-11-17 17:32:23 +0800

    最长公共子序列

--- nowcoder_最长公共子序列.cpp
+++ nowcoder_最长公共子序列.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
-    /**
-     * longest common substring
-     * @param str1 string字符串 the string
-     * @param str2 string字符串 the string
-     * @return string字符串
-     */
-    string LCS(string str1, string str2) {
-        // write code here
-        int len1 = str1.size();
-        int len2 = str2.size();
-        if(!len1 || !len2)
-            return "-1";
-        
-        int dp[len1+1][len2+1];
-        for(int i = 0; i <= len1; i++) {
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.size();
+        int len2 = text2.size();
+        if (!len1 || !len2)
+            return 0;
+
+        int dp[len1 + 1][len2 + 1];
+        for (int i = 0; i <= len1; i++) {
             dp[i][0] = 0;
         }
-        for(int i = 0; i <= len2; i++) {
+        for (int i = 0; i <= len2; i++) {
             dp[0][i] = 0;
         }
-        int max = 0;
-        int start = 0;
-        for(int i = 1; i <= len1; i++) {
-            for(int j = 1; j <= len2; j++) {
-                if(str1[i-1] == str2[j-2]) {
-                    dp[i][j] = dp[i-1][j-1] +1;
-                    
+
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1[i - 1] == text2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+
                 }
                 else {
-                    dp[i][j] = 0;
-                }
-                if(dp[i][j] > max) {
-                    max = dp[i][j];
-                    start = i - max;
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        
-        return str1.substr(start,max);
+
+        return dp[len1][len2];
     }
 };
