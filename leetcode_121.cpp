4e4bb9bb68b7e79561c02cc01cc4249d012728b5
BETTERGXC <2542602168@qq.com>
2021-03-04 17:08:40 +0800

    买卖股票的最佳时机【动态规划】

--- leetcode_121.cpp
+++ leetcode_121.cpp
@@ -14,6 +14,8 @@ public:
 };
 
 // 动态规划 
+//dp[i][0] 表示第 i 天持有股票
+//dp[i][1] 表示第 i 天不持有股票 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
@@ -22,7 +24,9 @@ public:
         dp[0][0] -= prices[0];
         dp[0][1] = 0;
         for(int i = 1; i < prices.size(); i++) {
+        	//dp[i][0] 要么保持前一天持有的状态，要么就今天持有 
             dp[i][0] = max(dp[i-1][0], -prices[i]);
+            //dp[i][1] 要么保持前一天不持有的状态，要么今天卖出 
             dp[i][1] = max(dp[i-1][1], prices[i] + dp[i-1][0]);
         }
 
