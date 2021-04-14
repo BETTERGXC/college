3a1c094fab5ceac296f0af398f251bdcc29c8e73
BETTERGXC <2542602168@qq.com>
2021-04-14 19:46:00 +0800

    组合【回溯法】

---
+++ leetcode_77.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    void backtrack(int n, int k, int start) {
+        if(path.size() == k) {
+            res.push_back(path);
+            return;
+        }
+        for(int i = start; i <= n; i++) {
+            path.push_back(i);
+            backtrack(n, k, i+1);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtrack(n, k, 1);
+        return res;
+    }
+};
